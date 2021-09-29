c002935e0b168edef6827be25d08b126e3e7a04b
Patrick Thurner <patrick.thurner@gmail.com>
2021-09-29 16:21:47 +0200

    Refactor Functions using Startup function to initialize TraceProvider.

--- AzFuncQueueDemo.cs
+++ AzFuncQueueDemo.cs
@@ -14,171 +14,124 @@ using OpenTelemetry.Context.Propagation;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Exporter;
+using AzFuncQueueDemo;
 
 namespace OpenTelemetryDemo
 {
-    public static class AzFuncQueueDemo
+    public class AzFuncQueueDemo
     {
-        //env-var to pick url to call web-triggered function 
-        private const string envWebTriggerUrl = "WebTriggerUrl";
 
         //env-var to pick Queue ConnectionString
         private const string envSBConnectionStr = "SBConnection";
+        private const string QueueName = "workitems";
 
-        //Defines the OpenTelemetry resource attribute "service.name" which is mandatory
-        private const string servicename = "AzFuncQueueDemo";
-
-        //Defines the OpenTelemetry Instrumentation Library.
-        private const string activitySource = "OpenTelemetryDemo.AzFuncQueueDemo";
-
-        //Provides the API for starting/stopping activities.
-        private static readonly ActivitySource myActivitySource = new ActivitySource(activitySource);
-
-        //message property used by servicebus client sdk to pass context
+        //message property used by servicebus client sdk to pass trace context
         private const string contextProperty = "Diagnostic-Id";
 
+        ActivitySource _activitySource;
+        TracerProvider _traceProvider;
 
-        private const string QueueName = "workitems";
-        //private const string QueueName = "workitems_svc";
-
-        private static string CollectorUrl
+        public AzFuncQueueDemo(ActivitySource activitySource, TracerProvider traceProvider)
         {
-            get { return Environment.GetEnvironmentVariable("COLLECTOR_URL") ?? "http://localhost:55680"; }
+            _activitySource = activitySource;
+            _traceProvider = traceProvider;
         }
 
         [FunctionName("TimerTrigger")]
-        public static async Task RunTrigger([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task RunTrigger([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            using (Sdk.CreateTracerProviderBuilder()
-            .SetSampler(new AlwaysOnSampler())
-            .AddSource(activitySource)
-            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(servicename))
-            .AddHttpClientInstrumentation()
-            .AddOtlpExporter(otlpOptions =>
+            //create root-span
+            using (var activity = _activitySource.StartActivity("Trigger", ActivityKind.Server))
             {
-                otlpOptions.Endpoint = new Uri(CollectorUrl);
-            })
-            .Build())
-            {
-                //create root-span
-                using (var activity = myActivitySource.StartActivity("Trigger", ActivityKind.Server))
-                {
 
-                    var client = new HttpClient();
+                var client = new HttpClient();
 
-                    //should be done automatically with .AddHttpClientInstrumentation(),
-                    //but doesn't work in Azure Consumption Plan.
-                    //Tested with Host Runtime 3.1 and 3.2
-                    client.DefaultRequestHeaders.Add("traceparent",activity.Id);
+                //HttpClient requires custom context propagation: https://github.com/Azure/azure-functions-host/issues/7135
+                if (activity != null)
+                    client.DefaultRequestHeaders.Add("traceparent", activity.Id);
 
-                    var targetUrl = Environment.GetEnvironmentVariable(envWebTriggerUrl) ??"http://localhost:7071/api/WebTrigger";
-                    
-                    var res = await client.GetAsync(targetUrl);
-                    
-                    if (res.IsSuccessStatusCode)
-                        log.LogInformation("Successfully called WebTrigger");
-                    else
-                        log.LogWarning("Failed calling WebTrigger");
-                    
-                }
+                var targetUrl = Environment.GetEnvironmentVariable("WebTriggerUrl") ?? "http://localhost:7071/api/WebTrigger";
+
+                var res = await client.GetAsync(targetUrl);
+
+                if (res.IsSuccessStatusCode)
+                    log.LogInformation("Successfully called WebTrigger");
+                else
+                    log.LogWarning("Failed calling WebTrigger");
+
             }
 
         }
 
 
         [FunctionName("WebTrigger")]
-        public static async Task<IActionResult> RunWebTrigger([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        public async Task<IActionResult> RunWebTrigger([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
         {
-
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            using (Sdk.CreateTracerProviderBuilder()
-            .SetSampler(new AlwaysOnSampler())
-            .AddSource(activitySource)
-            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(servicename))
-            .AddOtlpExporter(otlpOptions =>
+            using (var activity = _activitySource.StartActivity("WebTrigger", ActivityKind.Producer, req.Headers["traceparent"]))
             {
-                otlpOptions.Endpoint = new Uri(CollectorUrl);
-            })
-            .Build())
-            {
-                
-                //create root-span, connecting with trace-parent read from the http-header
-                using ( myActivitySource.StartActivity("WebTrigger", ActivityKind.Server, req.Headers["traceparent"]))
-                {
-                    
-                    using (var activity = myActivitySource.StartActivity("Producer", ActivityKind.Producer))
-                    {
-                        //follow semantic conventions for messaging
-                        activity.AddTag("peer.service", "ServiceBus");
-                        activity.AddTag("messaging.system", "ServiceBus");
-                        activity.AddTag("messaging.destination", QueueName);
-                        activity.AddTag("messaging.destination_kind", "queue");
+                //follow semantic conventions for messaging: https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
+                activity?.AddTag("peer.service", "ServiceBus");
+                activity?.AddTag("messaging.system", "ServiceBus");
+                activity?.AddTag("messaging.destination", QueueName);
+                activity?.AddTag("messaging.destination_kind", "queue");
 
-                        var client = new ServiceBusClient(Environment.GetEnvironmentVariable(envSBConnectionStr));
+                var client = new ServiceBusClient(Environment.GetEnvironmentVariable(envSBConnectionStr));
 
-                        //client framework automatically tags message. no need for custom propagation
-                        var sender = client.CreateSender(QueueName);
+                //client framework automatically tags message. no need for custom propagation
+                var sender = client.CreateSender(QueueName);
 
-                        // Create a new message to send to the queue
-                        var message = new ServiceBusMessage($"Message {DateTime.Now}");
-                        log.LogInformation($"Sending message: {message}");
+                // Create a new message to send to the queue
+                var message = new ServiceBusMessage($"Message {DateTime.Now}");
+                log.LogInformation($"Sending message: {message}");
 
-                        // Send the message to the queue
-                        await sender.SendMessageAsync(message);
-                    }
-                    
-                }
+                // Send the message to the queue
+                await sender.SendMessageAsync(message);
             }
 
-
             return new OkObjectResult("Ok");
-         
-        
      
         }
-
         
         
         [FunctionName("Consumer")]
-        public static async Task RunConsumer([ServiceBusTrigger(QueueName, Connection = envSBConnectionStr)] Message myQueueItem, ILogger log)
+        public async Task RunConsumer([ServiceBusTrigger(QueueName, Connection = envSBConnectionStr)] Message myQueueItem, ILogger log)
         {
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            string tracecontext="";
+            if (myQueueItem.UserProperties.ContainsKey(contextProperty))
+            {
+                tracecontext = myQueueItem.UserProperties[contextProperty] as string;
+                log.LogInformation($"C# Queue trigger function processed: {myQueueItem} with tracecontext '{tracecontext}'");
+            }
+            else
+                log.LogInformation($"C# Queue trigger function processed: {myQueueItem} without tracecontext");
 
-            using (Sdk.CreateTracerProviderBuilder()
-             .SetSampler(new AlwaysOnSampler())
-             .AddSource(activitySource)
-             .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(servicename))
-            .AddOtlpExporter(otlpOptions =>
+            //create root-span, connecting with trace-parent read from a message property
+            using (var activity = _activitySource.StartActivity("Consumer", ActivityKind.Consumer, tracecontext))
             {
-                otlpOptions.Endpoint = new Uri(CollectorUrl);
-            })
-            .Build())
-            {
-                string tracecontext="";
-                if (myQueueItem.UserProperties.ContainsKey(contextProperty))
+                //follow semantic conventions for messaging: https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
+                activity?.AddTag("peer.service", "ServiceBus");
+                activity?.AddTag("messaging.system", "ServiceBus");
+                activity?.AddTag("messaging.destination", QueueName);
+                activity?.AddTag("messaging.destination_kind", "queue");
+
+
+                //call another service which is instrumented with OneAgent
+                var outboundServiceUrl = Environment.GetEnvironmentVariable("OutboundServiceUrl");
+                if (!String.IsNullOrEmpty(outboundServiceUrl))
                 {
-                    tracecontext = myQueueItem.UserProperties[contextProperty] as string;
-                    log.LogInformation($"C# Queue trigger function processed: {myQueueItem} with tracecontext '{tracecontext}'");
+                    var client = new HttpClient();
+                    //HttpClient requires custom context propagation: https://github.com/Azure/azure-functions-host/issues/7135
+                    if (activity != null)
+                        client.DefaultRequestHeaders.Add("traceparent", activity.Id);
+
+                    var res = await client.GetAsync(outboundServiceUrl);
+                    if (res.IsSuccessStatusCode)
+                        log.LogInformation("Successfully called outbound Service");
+                    else
+                        activity.AddEvent(new ActivityEvent("Unable to call outbound service. Service returned an error"));
                 }
-                else
-                    log.LogInformation($"C# Queue trigger function processed: {myQueueItem} without tracecontext");
-
-                //create root-span, connecting with trace-parent read from a message property
-                using (var activity = myActivitySource.StartActivity("Consumer", ActivityKind.Consumer, tracecontext))
-                {
-                    //follow semantic conventions for messaging
-                    activity.AddTag("peer.service", "ServiceBus");
-                    activity.AddTag("messaging.system", "ServiceBus");
-                    activity.AddTag("messaging.destination", QueueName);
-                    activity.AddTag("messaging.destination_kind", "queue");
-
-                    log.LogInformation("Do something");
-                    await Task.Delay(200);
                    
-                }
-
             }
 
         }
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(AzFuncQueueDemo.Startup))]
+
+namespace AzFuncQueueDemo
+{
+    public class Startup : FunctionsStartup
+    {
+        //Defines the OpenTelemetry resource attribute "service.name" which is mandatory
+        private const string servicename = "AzFuncQueueDemo";
+
+        //Defines the OpenTelemetry Instrumentation Library.
+        private const string activitySource = "OpenTelemetryDemo.AzFuncQueueDemo";
+  
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //Using insecure channel: https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#special-case-when-using-insecure-channel
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            builder.Services.AddSingleton<ActivitySource>((s) => {
+                return new ActivitySource(activitySource);
+            });
+
+            //Do not use AddOpenTelemetryTracing (https://github.com/open-telemetry/opentelemetry-dotnet/issues/1803#issuecomment-800608308)
+            builder.Services.AddSingleton((builder) =>
+            {
+                return Sdk.CreateTracerProviderBuilder()
+                    .AddSource(activitySource)
+                    .SetSampler(new AlwaysOnSampler())
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(servicename))
+                    //.AddHttpClientInstrumentation() doesn't work:  https://github.com/Azure/azure-functions-host/issues/7135
+                    .AddOtlpExporter(otlpOptions =>
+                    {
+                        otlpOptions.Endpoint = new Uri(Environment.GetEnvironmentVariable("CollectorUrl") ?? "http://localhost:55680");
+                    }).Build();
+            });
+            
+        }
+    }
+}
